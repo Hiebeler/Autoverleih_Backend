a1af77581f09ebce9cb9de42ac6d70a02ad9fff4
Emanuel Hiebeler <emanuel.hiebeler@gmail.com>
2024-03-22 14:23:10 +0100

    added gitignore

--- Autoverleih_Backend/Models/CreateCarRequest.cs
+++ Autoverleih_Backend/Models/CreateCarRequest.cs
@@ -2,5 +2,9 @@ namespace Autoverleih_Backend.Models;
 
 public class CreateCarRequest
 {
-    
+    public int Seats { get; set; }
+    public string CarBrand { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    public int TrunkSpace { get; set; }
+    public CarTypes Type { get; set; }
 }
--- Autoverleih_Backend/Services/CarService.cs
+++ Autoverleih_Backend/Services/CarService.cs
@@ -1,11 +1,26 @@
 using Autoverleih_Backend.Models;
+using urlaubsplanungstool_backend.Db.Repositories;
 
 namespace Autoverleih_Backend.Services;
 
 public class CarService: ICarService
 {
-    public Task<Car?> CreateCar(CreateCarRequest createCarRequest)
+    private readonly IGenericRepository<Car> _genericRepository;
+    public CarService(IGenericRepository<Car> genericRepository)
     {
-        throw new NotImplementedException();
+        _genericRepository = genericRepository;
+    }
+    public async Task<Car?> CreateCar(CreateCarRequest createCarRequest)
+    {
+        Car newCar = new Car()
+        {
+            Type = createCarRequest.Type,
+            Model = createCarRequest.Model,
+            Seats = createCarRequest.Seats,
+            CarBrand = createCarRequest.CarBrand,
+            TrunkSpace = createCarRequest.TrunkSpace
+        };
+        var worked = await _genericRepository.Add(newCar);
+        return worked;
     }
 }
